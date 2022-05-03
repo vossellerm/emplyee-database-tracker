82437f31e049c05e23cfce2bb07c73612f9f1686
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 15:49:08 -0700

    seeds.sql data

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+("Finance"),
+("Legal"),
+("Sales");
+
+INSERT INTO role (title, salary)
+VALUES ("Sales Lead", 100000),
+("Salesperson", 80000),
+("Lead Engineer", 150000),
+("Software Engineer", 120000),
+("Account Manager", 160000),
+("Accountant", 125000),
+("Legal Team Lead", 250000),
+("Lawyer", 190000);
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ("John", "Doe"),
+("Mike", "Chan", 1),
+("Ashley", "Rodriguez"),
+("Kevin", "Tupik", 3),
+("Kunal", "Singh" ),
+("Malia", "Brown", 5),
+("Sarah", "Lourd"),
+("Tom", "Allen", 7);
