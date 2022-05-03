717dfd1b4e392f2db7c519739865c1e5dd29bb46
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 15:12:20 -0700

    schema.sql psuedocode

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- Create table Department
+-- Primary key id
+
+
+-- Create table Role
+-- Primary key id
+-- Foreign key department_id
+
+
+-- Create table Emplpoyee
+-- Primary key id
+-- Foreign key role_id
+-- Foreign key manager_id
