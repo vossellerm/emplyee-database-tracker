e9516fb5f05b21efd8d70615e4001d98f3d4a932
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 15:24:08 -0700

    schema.sql create tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,39 @@ USE employees_db;
 
 -- Create table Department
 -- Primary key id
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
 
 
 -- Create table Role
 -- Primary key id
 -- Foreign key department_id
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+)
 
-
--- Create table Emplpoyee
+-- Create table Employee
 -- Primary key id
 -- Foreign key role_id
 -- Foreign key manager_id
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+)
