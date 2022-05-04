1d96f80ace693ecf84577681c15bfe87852acbfe
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 21:10:31 -0700

    update seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ USE employees_db;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
 
 -- Create table Role
@@ -22,7 +22,7 @@ CREATE TABLE role (
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
 
 -- Create table Employee
 -- Primary key id
@@ -40,4 +40,4 @@ CREATE TABLE employee (
     FOREIGN KEY (manager_id)
     REFERENCES role(id)
     ON DELETE SET NULL
-)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,22 +4,22 @@ VALUES ("Engineering"),
 ("Legal"),
 ("Sales");
 
-INSERT INTO role (title, salary)
-VALUES ("Sales Lead", 100000),
-("Salesperson", 80000),
-("Lead Engineer", 150000),
-("Software Engineer", 120000),
-("Account Manager", 160000),
-("Accountant", 125000),
-("Legal Team Lead", 250000),
-("Lawyer", 190000);
+INSERT INTO role (department_id, title, salary)
+VALUES (4, "Sales Lead", 100000),
+(4, "Salesperson", 80000),
+(1, "Lead Engineer", 150000),
+(1, "Software Engineer", 120000),
+(2, "Account Manager", 160000),
+(2, "Accountant", 125000),
+(3, "Legal Team Lead", 250000),
+(3, "Lawyer", 190000);
 
-INSERT INTO employee (first_name, last_name, manager_id)
-VALUES ("John", "Doe"),
-("Mike", "Chan", 1),
-("Ashley", "Rodriguez"),
-("Kevin", "Tupik", 3),
-("Kunal", "Singh" ),
-("Malia", "Brown", 5),
-("Sarah", "Lourd"),
-("Tom", "Allen", 7);
+INSERT INTO employee (role_id, first_name, last_name, manager_id)
+VALUES (1, "John", "Doe", NULL),
+(2, "Mike", "Chan", 1),
+(3, "Ashley", "Rodriguez", NULL),
+(4, "Kevin", "Tupik", 3),
+(5, "Kunal", "Singh", NULL),
+(6, "Malia", "Brown", 5),
+(7, "Sarah", "Lourd", NULL),
+(8, "Tom", "Allen", 7);
