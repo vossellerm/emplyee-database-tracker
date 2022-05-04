b980f4c452344dd4debde112765d13f864525252
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 23:15:05 -0700

    install console.table

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const inquirer = require("inquirer");
 const express = require("express");
 const mysql = require("mysql2");
+const cTable = require("console.table");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "employees_db",
+});
