7b50a2abcd4d06a89597efdd29e2c3e08eec715a
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 23:05:09 -0700

    query.sql shows all tables

--- db/query.sql
+++ db/query.sql
@@ -14,8 +14,7 @@ ORDER BY role.id;
 
 -- view all employees
 SELECT employee.id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary, employee.manager_id AS manager
-FROM employee 
+FROM employee
 INNER JOIN role ON role.id = employee.role_id
 INNER JOIN department ON department.id = role.department_id
--- JOIN role ON role.id = employee.manager_id
 ORDER BY employee.id;
