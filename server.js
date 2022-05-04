4029c6a0d8d2bf0b85b38965eb9a4f1416466aba
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 23:22:08 -0700

    remove express

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const inquirer = require("inquirer");
-const express = require("express");
 const mysql = require("mysql2");
 const cTable = require("console.table");
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection({
