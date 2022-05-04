9a4515be806731283354f0b0b592c10bfba385df
Mikayla Vosseller <vossellerm@gmail.com>
2022-05-03 22:29:53 -0700

    query.sql view all employees without manager name

--- db/query.sql
+++ db/query.sql
@@ -12,3 +12,10 @@ FROM role JOIN department
 ON role.department_id = department.id
 ORDER BY role.id;
 
+-- view all employees
+SELECT employee.id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary, employee.manager_id AS manager
+FROM employee 
+INNER JOIN role ON role.id = employee.role_id
+INNER JOIN department ON department.id = role.department_id
+-- JOIN role ON role.id = employee.manager_id
+ORDER BY employee.id;
